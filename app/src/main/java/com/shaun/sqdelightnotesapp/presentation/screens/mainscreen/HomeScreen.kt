e55c679d83ff0fbd9e092fca3df1d99c3bb1e049
Sunny <30768018+sunny52525@users.noreply.github.com>
2021-10-22 20:21:49 +0530

    no notes warning

--- app/src/main/java/com/shaun/sqdelightnotesapp/presentation/screens/mainscreen/HomeScreen.kt
+++ app/src/main/java/com/shaun/sqdelightnotesapp/presentation/screens/mainscreen/HomeScreen.kt
@@ -2,15 +2,19 @@ package com.shaun.sqdelightnotesapp.presentation.screens.mainscreen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.shaun.sqdelightnotesapp.Notes
 import com.shaun.sqdelightnotesapp.presentation.components.NotesItem
@@ -25,20 +29,29 @@ fun HomeScreen(mainViewModel: MainViewModel,onNotesClick:(Notes)->Unit) {
 
     val notes: List<Notes> by mainViewModel.getNotes().collectAsState(listOf())
 
+    if (notes.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "No notes added,Click on the plus button to start", color = Color.White)
+        }
+    } else
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .background(KeepGray)
+        ) {
 
-    Column(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize()
-            .background(KeepGray)
-    ) {
-        StaggeredVerticalGrid(maxColumnWidth = 300.dp) {
-            notes.forEach { note ->
-                NotesItem(body = note.body, color = note.background_color) {
-                    onNotesClick(note)
+            StaggeredVerticalGrid(maxColumnWidth = 300.dp) {
+                notes.forEach { note ->
+                    NotesItem(body = note.body, color = note.background_color) {
+                        onNotesClick(note)
+                    }
                 }
             }
         }
 
-    }
+
 }
